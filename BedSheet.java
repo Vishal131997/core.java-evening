f8063721877dc1026284283dac4d9542f4161ef6
Vishal131997 <vishalcb.xworkz@gmail.com>
2022-11-23 20:49:36 +0530

    committing java files

---
+++ BedSheet.java
@@ -0,0 +1,25 @@
+class BedSheet{
+
+	public static void main (String Bedsheet[]){
+	
+	int roomNo = 14;
+	String floreNo= "3rd flore";
+	String roomType= "Double Bed";
+	String bedColor = "red";
+	int roomRent = 1000;
+	long contactNo = 9181057681L;
+	
+	
+
+	System.out.print("Well Come to \nBed Room");
+	System.out.println(roomNo);
+	System.out.println (floreNo);
+	System.out.println(roomType);
+	System.out.println(bedColor);
+	System.out.println(roomRent);
+	System.out.println(contactNo);
+	
+	}
+
+
+}
