8bdeda769673386f6378f5cb633a8e171c47a308
Vishal131997 <vishalcb.xworkz@gmail.com>
2022-11-25 20:11:34 +0530

    Committing methods related java files

---
+++ Add.java
@@ -0,0 +1,21 @@
+class Add{
+	public static void main(String args[]){
+	
+	add(2, 3, 3);
+	add(5, 3, 45);
+	add(6, 77, 65);
+	add(98, 89, 365);}
+	
+	public static void add (int number1, int number2, int number3){
+	
+	System.out.println(number1 + number2);
+	
+	
+	
+	}
+	
+	
+	
+	
+	}
+	
---
+++ Addi.java
@@ -0,0 +1,20 @@
+class Addi{
+
+	public static void  main(String Addi[]){
+	
+	//invoking a method / calling method
+	//arguments are 2, 3, 30
+	int total = add(2, 3, 30);
+	int total =add(3, 5, 8);
+	int total = add(6, 7, 9);
+	int total = add(9, 7, 3);}
+	
+	public static int add(int number1, int number2, int number3){
+	//System.out.println(number1*number2*number3);
+	
+	return number1 + number2;
+	
+	}
+
+
+}
